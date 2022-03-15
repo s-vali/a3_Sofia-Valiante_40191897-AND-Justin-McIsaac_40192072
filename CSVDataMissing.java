508a43e3ff7236c3825326fd97dc38c2930225a3
s-vali <91510546+s-vali@users.noreply.github.com>
2022-03-14 21:33:08 -0400

    Add files via upload

--- CSVDataMissing.java
+++ CSVDataMissing.java
@@ -1,6 +1,57 @@
+import java.io.File;
+import java.io.PrintWriter;
+
 /*
  * Exception Class
  */
 
-public class CSVDataMissing {	
+public class CSVDataMissing extends Exception {	
+
+	public CSVDataMissing(File currentFile, PrintWriter writeToExceptionsLog, String[] data, int lineNb) {
+		
+		//Print out error message to console
+		super("Error: Input row cannot be parsed due to missing information. ");
+		
+		//Print error message to log file
+		String dataAttribute = "";
+		if(currentFile.getName().compareTo("covidStatistics-CSV format.csv") == 0) {
+			try {
+				if(data[0].compareToIgnoreCase("Age Group") != 0) {
+					dataAttribute = "Age Group";
+				}
+				else if(data[1].compareToIgnoreCase("Hospitalized") != 0) {
+					dataAttribute = "Hospitalized";
+				}
+				else if(data[2].compareToIgnoreCase("ICU") != 0){
+					dataAttribute = "ICU";
+				}
+				else if(data[3].compareToIgnoreCase("Fully Vaccinated") != 0){
+					dataAttribute = "Fully Vaccinated";
+				}
+			}
+			catch(ArrayIndexOutOfBoundsException e) {
+				dataAttribute = "Fully Vaccinated";
+			}	
+		}
+		if(currentFile.getName().compareTo("doctorList-CSVformat.csv") == 0) {
+			try {
+				if(data[0].compareToIgnoreCase("Doctor") != 0) {
+					dataAttribute = "Doctor";
+				}
+				else if(data[1].compareToIgnoreCase("Specialty") != 0) {
+					dataAttribute = "Specialty";
+				}
+				else if(data[2].compareToIgnoreCase("Patient Line") != 0){
+					dataAttribute = "Patient Line";
+				}
+				else if(data[3].compareToIgnoreCase("Office Number") != 0){
+					dataAttribute = "Office Number";
+				}
+			}
+			catch(ArrayIndexOutOfBoundsException e) {
+				dataAttribute = "Office Number";
+			}
+		}
+		writeToExceptionsLog.println("WARNNING: In file " + currentFile.getName() + " line " + lineNb + " is not converted to HTML: missing data: " + dataAttribute + ". ");
+	}
 }
